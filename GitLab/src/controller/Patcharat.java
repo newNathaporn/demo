09d081c0d4778e2bad0d868ea20e876fbad6e401
CSTUROOM111 <CSTUROOM111@CSTURoom111-47>
2017-11-08 22:57:01 +0700

    Edit Message from Pat

--- GitLab/src/controller/Patcharat.java
+++ GitLab/src/controller/Patcharat.java
@@ -5,8 +5,8 @@ import javax.swing.JOptionPane;
 public class Patcharat {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Hello from Patcharat + Songsakdi!!");
-		JOptionPane.showMessageDialog(null, "My Second version from Songsakdi.");
+		JOptionPane.showMessageDialog(null, "Hello from Miss Patcharat + Songsakdi!!");
+		JOptionPane.showMessageDialog(null, "My Second version from Miss Patcharat.");
 
 	}
 
