c6ae44e2d84c774a4654c29e71641a4e0aa746d6
CSTUROOM111 <CSTUROOM111@CSTURoom111-47>
2017-11-08 19:14:18 +0700

    Add New class patcharat

---
+++ GitLab/src/controller/Patcharat.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javax.swing.JOptionPane;
+
+public class Patcharat {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello from Patcharat!!");
+		
+
+	}
+
+}
